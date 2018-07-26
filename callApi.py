d00d12cfb8120a03408a9c9816882a2e21a4864a
Rick Van <x.rckvan@gmail.com>
2018-07-26 13:13:39 -0600

    installing pip and requests, learning about headers in python

--- callApi.py
+++ callApi.py
@@ -1,1 +1,14 @@
 # Creating a simple AJAX call
+import sys
+import requests
+
+# parameters = {
+#     'lat': 40.71,
+#     'lon':-74
+# }
+# response = requests.get('http://api.open-notify.org/iss-pass.json', params=parameters)
+
+# data = response.json()
+# # print(type(data))
+# # print(data)
+# print(response.headers['Content-Type'])
